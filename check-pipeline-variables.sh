0a94cbdf5d5e1af05f4b41bda44bb3ad1438becd
Niek Nijland <niek.nijland@dij.digital>
2023-11-08 11:18:36 +0100

    initial commit

---
+++ check-pipeline-variables.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+envContents=$(<.env.production.dist)
+yamlContents=$(<bitbucket-pipelines.yml)
+
+contents="$envContents$yamlContents"
+
+names=()
+
+while read -r line; do
+    if [[ $line =~ \$\{[\w]*\} ]]; then
+        lastPart="${line#*\{}"
+        name="${lastPart%\}}"
+        names+=("$name")
+    fi
+done <<< "$contents"
+
+result=$(printenv)
+
+IFS=$'\n' read -ra envLines <<< "$result"
+
+for i in "${!envLines[@]}"; do
+    envLines[$i]=${envLines[$i%%=*]}
+done
+
+missing=()
+ignoredNames=('APP_KEY' 'APP_NAME')
+
+for name in "${names[@]}"; do
+    if [[ " ${ignoredNames[@]} " =~ " $name " ]]; then
+        continue
+    fi
+
+    if [[ ! " ${envLines[@]} " =~ " $name " ]]; then
+        missing+=("$name")
+    fi
+done
+
+if [ ${#missing[@]} -gt 0 ]; then
+    echo "Missing variables: ${missing[@]}"
+    exit 1
+fi
+
