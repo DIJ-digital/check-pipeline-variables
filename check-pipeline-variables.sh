bdabc3b4bf39edf3c262760f0de785405db3ed64
Tycho Engberink <tycho.engberink@dij.digital>
2023-11-24 11:20:45 +0100

    Add support for env file as argument

--- check-pipeline-variables.sh
+++ check-pipeline-variables.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
 
-envContents=$(<.env.example)
+# Check if an argument is provided
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <environment-file>"
+    exit 1
+fi
+
+# Use the first argument as the environment file
+envFile=$1
+
+# Read contents of the provided environment file
+if [ -f "$envFile" ]; then
+    envContents=$(<"$envFile")
+else
+    echo "Environment file not found: $envFile"
+    exit 1
+fi
+
 yamlContents=$(<bitbucket-pipelines.yml)
 
 contents="$envContents$yamlContents"
 
-names=()
+# Initialize the names array properly
+declare -a names
 
 while read -r line; do
     if [[ $line =~ \$\{[\w]*\} ]]; then
@@ -19,11 +36,13 @@ result=$(printenv)
 
 IFS=$'\n' read -ra envLines <<< "$result"
 
+# Process the envLines array
 for i in "${!envLines[@]}"; do
     envLines[$i]=${envLines[$i]%%=*}
 done
 
-missing=()
+# Initialize the missing array properly
+declare -a missing
 ignoredNames=('APP_KEY' 'APP_NAME')
 
 for name in "${names[@]}"; do
@@ -40,4 +59,3 @@ if [ ${#missing[@]} -gt 0 ]; then
     echo "Missing variables: ${missing[@]}"
     exit 1
 fi
-
