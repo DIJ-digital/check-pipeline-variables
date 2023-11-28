b17a995ccb33d6f489727488ec2a43bb7e549b4a
Tycho Engberink <tycho.engberink@dij.digital>
2023-11-28 14:08:40 +0100

    Debugging script

--- check-pipeline-variables.sh
+++ check-pipeline-variables.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+echo "Starting the script..."
+
 # Check if file argument is provided
 if [ "$#" -ne 1 ]; then
     echo "Usage: $0 <env-file>"
     exit 1
 fi
+
+echo "Argument provided: $1"
 
 # Get the file from the first argument
 envFile="$1"
@@ -15,19 +19,24 @@ if [ ! -f "$envFile" ]; then
     exit 1
 fi
 
+echo "Reading environment file: $envFile"
+
 # Read contents of the environment file
 envContents=$(cat "$envFile")
 
 # Extract environment variable names from contents
+echo "Extracting environment variable names from file content..."
 names=()
 for line in $envContents; do
     if [[ $line =~ \$\{[a-zA-Z_][a-zA-Z0-9_]*\} ]]; then
         name=$(echo $line | grep -oP '\$\{\K[a-zA-Z_][a-zA-Z0-9_]*(?=\})')
         names+=($name)
+        echo "Found variable: $name"
     fi
 done
 
 # Get all environment variables
+echo "Fetching all environment variables..."
 envLines=$(printenv | cut -d '=' -f1)
 
 # Check for missing variables
@@ -35,11 +44,14 @@ missing=()
 ignoredNames=("APP_KEY" "APP_NAME")
 
 for name in ${names[@]}; do
+    echo "Checking variable: $name"
+
     # Check if the name is in ignoredNames
     ignore=false
     for ignoredName in ${ignoredNames[@]}; do
         if [ "$name" == "$ignoredName" ]; then
             ignore=true
+            echo "Ignoring variable: $name"
             break
         fi
     done
@@ -52,16 +64,17 @@ for name in ${names[@]}; do
     for env in $envLines; do
         if [ "$env" == "$name" ]; then
             found=true
+            echo "Variable found in environment: $name"
             break
         fi
     done
 
     # Add to missing only if it's in the names array
     if [ "$found" == false ]; then
-        if [[ ! " ${names[@]} " =~ " ${name} " ]]; then
-            # Check if the name is already in the missing array to avoid duplicates
+        if [[ " ${names[@]} " =~ " ${name} " ]]; then
             if [[ ! " ${missing[*]} " =~ " ${name} " ]]; then
                 missing+=($name)
+                echo "Missing variable: $name"
             fi
         fi
     fi
@@ -72,3 +85,5 @@ if [ ${#missing[@]} -gt 0 ]; then
     echo "Missing variables: ${missing[*]}"
     exit 1
 fi
+
+echo "Script execution completed successfully."
