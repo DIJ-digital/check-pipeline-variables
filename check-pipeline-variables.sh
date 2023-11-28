6f06f91bee52cfc34da11823c5c212158fd7f71b
Tycho Engberink <tycho.engberink@dij.digital>
2023-11-28 09:53:08 +0100

    Debug script

--- check-pipeline-variables.sh
+++ check-pipeline-variables.sh
@@ -1,40 +1,52 @@
 #!/bin/bash
 
+echo "Starting script..."
+
 # Check if an argument is provided
 if [ "$#" -ne 1 ]; then
     echo "Usage: $0 <environment-file>"
     exit 1
 fi
 
+echo "Argument provided: $1"
+
 # Use the first argument as the environment file
 envFile=$1
+
+echo "Reading environment file: $envFile"
 
 # Read contents of the provided environment file
 if [ -f "$envFile" ]; then
     envContents=$(<"$envFile")
+    echo "Environment file contents: $envContents"
 else
     echo "Environment file not found: $envFile"
     exit 1
 fi
 
+echo "Reading YAML contents from bitbucket-pipelines.yml"
 yamlContents=$(<bitbucket-pipelines.yml)
+echo "YAML contents: $yamlContents"
 
 contents="$envContents$yamlContents"
 
-echo "$contents"
-
 # Initialize the names array properly
 declare -a names
+
+echo "Parsing variables from contents"
 
 while read -r line; do
     if [[ $line =~ \$\{[\w]*\} ]]; then
         lastPart="${line#*\{}"
         name="${lastPart%\}}"
         names+=("$name")
+        echo "Found variable: $name"
     fi
 done <<< "$contents"
 
+echo "Getting environment variables"
 result=$(printenv)
+echo "Environment variables: $result"
 
 IFS=$'\n' read -ra envLines <<< "$result"
 
@@ -47,13 +59,17 @@ done
 declare -a missing
 ignoredNames=('APP_KEY' 'APP_NAME')
 
+echo "Checking for missing variables"
+
 for name in "${names[@]}"; do
     if [[ " ${ignoredNames[@]} " =~ " $name " ]]; then
+        echo "Ignoring variable: $name"
         continue
     fi
 
     if [[ ! " ${envLines[@]} " =~ " $name " ]]; then
         missing+=("$name")
+        echo "Missing variable: $name"
     fi
 done
 
@@ -63,3 +79,5 @@ if [ ${#missing[@]} -gt 0 ]; then
 else
     echo "No missing variables found."
 fi
+
+echo "Script completed."
