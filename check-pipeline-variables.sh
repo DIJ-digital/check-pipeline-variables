672d0655d5b78049629a53e412b9d8a74fe781c3
Tycho Engberink <tycho.engberink@dij.digital>
2023-11-28 14:04:49 +0100

    Reference checking

--- check-pipeline-variables.sh
+++ check-pipeline-variables.sh
@@ -17,22 +17,13 @@ fi
 
 # Read contents of the environment file
 envContents=$(cat "$envFile")
-yamlContents=$(cat bitbucket-pipelines.yml)
 
-# Combine contents
-contents="$envContents $yamlContents"
-
-# Extract environment variable names and references from contents
+# Extract environment variable names from contents
 names=()
-declare -A references
-for line in $contents; do
-    if [[ $line =~ ([a-zA-Z_][a-zA-Z0-9_]*)\=\$\{([a-zA-Z_][a-zA-Z0-9_]*)\} ]]; then
-        name="${BASH_REMATCH[1]}"
-        ref="${BASH_REMATCH[2]}"
-        references["$name"]="$ref"
-    elif [[ $line =~ \$\{[a-zA-Z_][a-zA-Z0-9_]*\} ]]; then
+for line in $envContents; do
+    if [[ $line =~ \$\{[a-zA-Z_][a-zA-Z0-9_]*\} ]]; then
         name=$(echo $line | grep -oP '\$\{\K[a-zA-Z_][a-zA-Z0-9_]*(?=\})')
-        names+=("$name")
+        names+=($name)
     fi
 done
 
@@ -43,21 +34,35 @@ envLines=$(printenv | cut -d '=' -f1)
 missing=()
 ignoredNames=("APP_KEY" "APP_NAME")
 
-for name in "${names[@]}"; do
+for name in ${names[@]}; do
     # Check if the name is in ignoredNames
-    if [[ " ${ignoredNames[@]} " =~ " ${name} " ]]; then
+    ignore=false
+    for ignoredName in ${ignoredNames[@]}; do
+        if [ "$name" == "$ignoredName" ]; then
+            ignore=true
+            break
+        fi
+    done
+
+    if [ "$ignore" == true ]; then
         continue
     fi
 
-    # Check if the variable or its reference exists
-    if [[ -n ${references["$name"]} ]]; then
-        ref=${references["$name"]}
-        if [[ ! " ${envLines[@]} " =~ " ${ref} " ]]; then
-            missing+=("$name")
+    found=false
+    for env in $envLines; do
+        if [ "$env" == "$name" ]; then
+            found=true
+            break
         fi
-    else
-        if [[ ! " ${envLines[@]} " =~ " ${name} " ]]; then
-            missing+=("$name")
+    done
+
+    # Add to missing only if it's in the names array
+    if [ "$found" == false ]; then
+        if [[ ! " ${names[@]} " =~ " ${name} " ]]; then
+            # Check if the name is already in the missing array to avoid duplicates
+            if [[ ! " ${missing[*]} " =~ " ${name} " ]]; then
+                missing+=($name)
+            fi
         fi
     fi
 done
