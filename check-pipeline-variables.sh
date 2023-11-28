62ab6c05163b874f739b2aba6c2886d72201f9fb
Tycho Engberink <tycho.engberink@dij.digital>
2023-11-28 13:47:35 +0100

    Reference checking

--- check-pipeline-variables.sh
+++ check-pipeline-variables.sh
@@ -15,23 +15,24 @@ if [ ! -f "$envFile" ]; then
     exit 1
 fi
 
-# Read contents of the environment file and YAML file
+# Read contents of the environment file
 envContents=$(cat "$envFile")
 yamlContents=$(cat bitbucket-pipelines.yml)
 
 # Combine contents
 contents="$envContents $yamlContents"
 
-# Extract environment variable names and their references from contents
-declare -A varReferences
+# Extract environment variable names and references from contents
+names=()
+declare -A references
 for line in $contents; do
-    if [[ $line =~ \$\{[a-zA-Z_][a-zA-Z0-9_]*\} ]]; then
+    if [[ $line =~ ([a-zA-Z_][a-zA-Z0-9_]*)\=\$\{([a-zA-Z_][a-zA-Z0-9_]*)\} ]]; then
+        name="${BASH_REMATCH[1]}"
+        ref="${BASH_REMATCH[2]}"
+        references["$name"]="$ref"
+    elif [[ $line =~ \$\{[a-zA-Z_][a-zA-Z0-9_]*\} ]]; then
         name=$(echo $line | grep -oP '\$\{\K[a-zA-Z_][a-zA-Z0-9_]*(?=\})')
-        # Check if there's a reference in the form VARIABLE=${REFERENCE}
-        if [[ $line =~ ([a-zA-Z_][a-zA-Z0-9_]*)\=\$\{([a-zA-Z_][a-zA-Z0-9_]*)\} ]]; then
-            refName=${BASH_REMATCH[2]}
-            varReferences[$name]=$refName
-        fi
+        names+=("$name")
     fi
 done
 
@@ -42,32 +43,22 @@ envLines=$(printenv | cut -d '=' -f1)
 missing=()
 ignoredNames=("APP_KEY" "APP_NAME")
 
-for name in "${!varReferences[@]}"; do
+for name in "${names[@]}"; do
     # Check if the name is in ignoredNames
-    ignore=false
-    for ignoredName in "${ignoredNames[@]}"; do
-        if [ "$name" == "$ignoredName" ]; then
-            ignore=true
-            break
-        fi
-    done
-
-    if [ "$ignore" == true ]; then
+    if [[ " ${ignoredNames[@]} " =~ " ${name} " ]]; then
         continue
     fi
 
-    # Check if the variable or its reference exists in the environment
-    found=false
-    ref=${varReferences[$name]}
-    for env in $envLines; do
-        if [ "$env" == "$name" ] || ([ ! -z "$ref" ] && [ "$env" == "$ref" ]); then
-            found=true
-            break
+    # Check if the variable or its reference exists
+    if [[ -n ${references["$name"]} ]]; then
+        ref=${references["$name"]}
+        if [[ ! " ${envLines[@]} " =~ " ${ref} " ]]; then
+            missing+=("$name")
         fi
-    done
-
-    if [ "$found" == false ]; then
-        missing+=($name)
+    else
+        if [[ ! " ${envLines[@]} " =~ " ${name} " ]]; then
+            missing+=("$name")
+        fi
     fi
 done
 
